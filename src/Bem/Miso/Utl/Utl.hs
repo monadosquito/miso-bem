34aa8a008bd5c65623cd33aff9d470696b43fe70
monadosquito <monadosquito@gmail.com>
2023-03-23 15:23:14 +0300

    refactor(utl): alias partial view makers
    The 'mkBlkNoModsElem', 'mkNoModsBlkElem', 'mkNoModsBlkNoModsElem',
    and 'noModsElem' partial view generators,
    defined in the 'Bem.Miso.Utl.Utl' module,
    using the default decorations
    are defined in terms of the configurable ones,
    defined in the 'Bem.Miso.Utl.Utl' module,
    configured with the 'defCfg' default configuration value,
    defined in the 'Bem.Cfg.Cfg' module,
    to get rid of the unnecessary code.

--- src/Bem/Miso/Utl/Utl.hs
+++ src/Bem/Miso/Utl/Utl.hs
@@ -15,7 +15,6 @@ import Miso
 
 import Bem.Miso.View.Html
 
-import Bem.Cls.Gen.Gen
 import Bem.Utl.Utl
 import Control.Monad.Reader
 import Miso.String
@@ -59,19 +58,7 @@ having a class of a block along with its modifiers
 and element without its modifiers.
 -}
 blkNoModsElem :: HtmlElem a c -> c -> FromBlkNoModsElem (View a)
-blkNoModsElem (NonVoidHtmlElem nonVoidHtmlElem) (attrs, views)
-    =
-    \blk blkMods prnt elem'
-    ->
-    nonVoidHtmlElem
-        ((class_ . ms $ genNoElemModsBlk blk blkMods prnt elem') : attrs)
-        views
-blkNoModsElem (VoidHtmlElem voidHtmlElem) attrs
-    =
-    \blk blkMods prnt elem'
-    ->
-    voidHtmlElem
-        $ (class_ . ms $ genNoElemModsBlk blk blkMods prnt elem') : attrs
+blkNoModsElem = _blkNoModsElem $ Bem.Miso.Utl.Utl.init defCfg
 
 {- |
 Make a view
@@ -80,23 +67,7 @@ and a class of a block along with its modifiers
 and element without its modifiers.
 -}
 mkBlkNoModsElem :: HtmlElem a c -> c -> Reader m (BlkNoModsElem a)
-mkBlkNoModsElem (NonVoidHtmlElem nonVoidHtmlElem) (attrs, views)
-    =
-    return
-        $ BlkNoModsElem
-        $ \blk blkMods prnt elem'
-          ->
-          nonVoidHtmlElem
-              ((class_ . ms $ genNoElemModsBlk blk blkMods prnt elem') : attrs)
-              views
-mkBlkNoModsElem (VoidHtmlElem voidHtmlElem) attrs
-    =
-    return
-        $ BlkNoModsElem
-        $ \blk blkMods prnt elem'
-          ->
-          voidHtmlElem
-              $ (class_ . ms $ genNoElemModsBlk blk blkMods prnt elem') : attrs
+mkBlkNoModsElem = _mkBlkNoModsElem $ Bem.Miso.Utl.Utl.init defCfg
 
 {- |
 Make a view
@@ -105,23 +76,7 @@ and a class of a block without its modifiers
 and element along with its modifiers.
 -}
 mkNoModsBlkElem :: HtmlElem a c -> c -> Reader m (NoModsBlkElem a)
-mkNoModsBlkElem (NonVoidHtmlElem nonVoidHtmlElem) (attrs, views)
-    =
-    return
-        $ NoModsBlkElem
-        $ \blk prnt elem' elemMods
-          ->
-          nonVoidHtmlElem
-              ((class_ . ms $ genNoBlkModsBlk blk prnt elem' elemMods) : attrs)
-              views
-mkNoModsBlkElem (VoidHtmlElem voidHtmlElem) attrs
-    =
-    return
-        $ NoModsBlkElem
-        $ \blk prnt elem' elemMods
-          ->
-          voidHtmlElem
-              $ (class_ . ms $ genNoBlkModsBlk blk prnt elem' elemMods) : attrs
+mkNoModsBlkElem = _mkNoModsBlkElem $ Bem.Miso.Utl.Utl.init defCfg
 
 {- |
 Make a view
@@ -130,22 +85,7 @@ and a class of a block
 and element without their modifiers.
 -}
 mkNoModsBlkNoModsElem :: HtmlElem a c -> c -> Reader m (NoModsBlkNoModsElem a)
-mkNoModsBlkNoModsElem (NonVoidHtmlElem nonVoidHtmlElem) (attrs, views)
-    =
-    return
-        $ NoModsBlkNoModsElem
-        $ \blk prnt elem'
-          ->
-          nonVoidHtmlElem
-              ((class_ . ms $ genNoModsBlk blk prnt elem') : attrs)
-              views
-mkNoModsBlkNoModsElem (VoidHtmlElem voidHtmlElem) attrs
-    =
-    return
-        $ NoModsBlkNoModsElem
-        $ \blk prnt elem'
-          ->
-          voidHtmlElem $ (class_ . ms $ genNoModsBlk blk prnt elem') : attrs
+mkNoModsBlkNoModsElem = _mkNoModsBlkNoModsElem $ Bem.Miso.Utl.Utl.init defCfg
 
 {- |
 Make a view
@@ -153,49 +93,15 @@ having a class of a block without its modifiers
 and element along with its modifiers.
 -}
 noModsBlkElem :: HtmlElem a c -> c -> FromNoModsBlkElem (View a)
-noModsBlkElem (NonVoidHtmlElem nonVoidHtmlElem) (attrs, views)
-    =
-    \blk prnt elem' elemMods
-    ->
-    nonVoidHtmlElem
-        ((class_ . ms $ genNoBlkModsBlk blk prnt elem' elemMods) : attrs)
-        views
-noModsBlkElem (VoidHtmlElem voidHtmlElem) attrs
-    =
-    \blk prnt elem' elemMods
-    ->
-    voidHtmlElem
-        $ (class_ . ms $ genNoBlkModsBlk blk prnt elem' elemMods) : attrs
+noModsBlkElem = _noModsBlkElem $ Bem.Miso.Utl.Utl.init defCfg
 
 -- | Make a view having a class of a block and element without their modifiers.
 noModsBlkNoModsElem :: HtmlElem a c -> c -> FromNoModsBlkNoModsElem (View a)
-noModsBlkNoModsElem (NonVoidHtmlElem nonVoidHtmlElem) (attrs, views)
-    =
-    \blk prnt elem'
-    ->
-    nonVoidHtmlElem
-        ((class_ . ms $ genNoModsBlk blk prnt elem') : attrs)
-        views
-noModsBlkNoModsElem (VoidHtmlElem voidHtmlElem) attrs
-    =
-    \blk prnt elem'
-    ->
-    voidHtmlElem $ (class_ . ms $ genNoModsBlk blk prnt elem') : attrs
+noModsBlkNoModsElem = _noModsBlkNoModsElem $ Bem.Miso.Utl.Utl.init defCfg
 
 -- | Make a view having a class of an element without its modifiers.
 noModsElem :: HtmlElem a c -> c -> forall b . FromElem b (View a)
-noModsElem (NonVoidHtmlElem nonVoidHtmlElem) (attrs, views)
-    =
-    \prnt elem'
-    ->
-    nonVoidHtmlElem
-        ((class_ . ms $ genElem prnt elem' []) : attrs)
-        views
-noModsElem (VoidHtmlElem nonVoidHtmlElem) attrs
-    =
-    \prnt elem'
-    ->
-    nonVoidHtmlElem $ (class_ . ms $ genElem prnt elem' []) : attrs
+noModsElem = _noModsElem $ Bem.Miso.Utl.Utl.init defCfg
 
 
 -- | a view having a class of a block and element along with their modifiers
